35c7097518eb2ec43eb9d4045a096ee9ae1a2766
soonmo <smkwon97@gmail.com>
2024-06-04 18:39:32 +0900

    feat: 회원가입(일반)

---
+++ server/src/main/java/project/developer_pacemaker/entity/UserEntity.java
@@ -0,0 +1,33 @@
+package project.developer_pacemaker.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int uSeq;
+
+    @Column(name="pw", nullable = true, length = 100)
+    private String pw;
+
+    @Column(name="nickname", nullable = false, length = 30)
+    private String nickname;
+
+    @Column(name="email", nullable = false, length = 50)
+    private String email;
+
+    @Column(name="img", nullable = true, length = 255)
+    private String img;
+
+    @Column(name="social", nullable = true, length = 20)
+    private String social;
+}
+
---
+++ server/src/main/java/project/developer_pacemaker/service/UserService.java
@@ -0,0 +1,41 @@
+package project.developer_pacemaker.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import project.developer_pacemaker.entity.UserEntity;
+import project.developer_pacemaker.repository.UserRepository;
+
+@Service
+@Slf4j
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public UserEntity create(final UserEntity userEntity) {
+        if (userEntity == null || userEntity.getEmail() == null || userEntity.getNickname() == null ||
+            userEntity.getEmail().trim().isEmpty() || userEntity.getNickname().trim().isEmpty()
+        ) {
+            throw new RuntimeException("Invalid arguments");
+        }
+
+        final String email = userEntity.getEmail();
+        final String nickname = userEntity.getNickname();
+
+        if (userRepository.existsByEmail(email)) {
+            log.warn("Email already exists {}", email);
+            throw new RuntimeException("Email already exists");
+        }
+
+        if (userRepository.existsByNickname(nickname)) {
+            log.warn("Nickname already exists {}", email);
+            throw new RuntimeException("Nickname already exists");
+        }
+
+        // UserEntity 를 DB에 저장.
+        // save를 했을 때 반환되는 객체는 Entity 객체
+        return userRepository.save(userEntity);
+    }
+
+}
