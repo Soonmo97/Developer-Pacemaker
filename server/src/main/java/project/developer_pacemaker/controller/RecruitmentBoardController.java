38a4b80b3bb4c03ab3920a24d1ea8e0bc02a6d3c
yhs0329 <scar0329@naver.com>
2024-06-14 20:17:39 +0900

    feat:스터디 모집 게시글 생성 시 사용자가 그룹장인 스터디 그룹 리스트 표시 기능 추가

--- server/src/main/java/project/developer_pacemaker/controller/RecruitmentBoardController.java
+++ server/src/main/java/project/developer_pacemaker/controller/RecruitmentBoardController.java
@@ -8,9 +8,11 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import project.developer_pacemaker.dto.RecruitmentBoardDTO;
 import project.developer_pacemaker.dto.ResErrorDTO;
+import project.developer_pacemaker.dto.StudyGroupDTO;
 import project.developer_pacemaker.entity.RecruitmentBoardEntity;
 import project.developer_pacemaker.entity.StudyGroupEntity;
 import project.developer_pacemaker.service.RecruitmentBoardService;
+import project.developer_pacemaker.service.StudyGroupService;
 
 import java.util.List;
 
@@ -20,10 +22,12 @@ import java.util.List;
 public class RecruitmentBoardController {
 
     final private RecruitmentBoardService recruitmentBoardService;
+
     @Autowired
     public RecruitmentBoardController(RecruitmentBoardService recruitmentBoardService) {
         this.recruitmentBoardService = recruitmentBoardService;
     }
+
     @Operation(summary = "스터디 모집 게시글 작성 {sg_seq}, {content}, {name}", description = "스터디 모집 게시판 작성 API 입니다. {sg_seq}, {content}, {name} | name이 사용자가 입력하는 제목입니다.")
     @PostMapping
     public ResponseEntity<?> createRecruitmentBoard(@AuthenticationPrincipal String uSeq, @RequestBody RecruitmentBoardDTO recruitmentBoardDTO) {
@@ -37,35 +41,50 @@ public class RecruitmentBoardController {
             );
         }
     }
+
     @Operation(summary = "스터디 모집 게시글 조회", description = "스터디 모집 게시판 조회 API 입니다.")
     @GetMapping
-    public List<RecruitmentBoardEntity> getAllRecruitmentBoards(){
+    public List<RecruitmentBoardEntity> getAllRecruitmentBoards() {
         return recruitmentBoardService.getAllRecruitmentBoards();
     }
+
     @Operation(summary = "스터디 모집 게시글 제목 검색", description = "스터디 모집 게시판 제목 검색 API 입니다.")
     @GetMapping("/search")
-    public ResponseEntity<List<RecruitmentBoardEntity>> getRecruitmentBoardByName(@RequestParam String name){
+    public ResponseEntity<List<RecruitmentBoardEntity>> getRecruitmentBoardByName(@RequestParam String name) {
         List<RecruitmentBoardEntity> recruitmentBoard = recruitmentBoardService.getRecruitmentBoardByName(name);
-        if(recruitmentBoard.isEmpty()){
+        if (recruitmentBoard.isEmpty()) {
             return ResponseEntity.notFound().build();
         } else {
             return ResponseEntity.ok(recruitmentBoard);
         }
     }
+
     @Operation(summary = "스터디 모집 게시글 수정", description = "스터디 모집 게시글 수정 API 입니다.")
     @PatchMapping("/{rb_seq}")
     public ResponseEntity<RecruitmentBoardEntity> updateRecruitmentBoard(
             @AuthenticationPrincipal String uSeq,
-            @PathVariable Long id,  // @PathVariable은 URL에서 {id} 에 해당하는 부분 추출
-            @RequestBody RecruitmentBoardDTO recruitmentBoardDetails)
-            {
-        return ResponseEntity.ok(recruitmentBoardService.updateRecruitmentBoard(id, recruitmentBoardDetails, uSeq));
+            @PathVariable Long rb_seq,  // @PathVariable은 URL에서 {id} 에 해당하는 부분 추출
+            @RequestBody RecruitmentBoardDTO recruitmentBoardDetails) {
+        return ResponseEntity.ok(recruitmentBoardService.updateRecruitmentBoard(rb_seq, recruitmentBoardDetails, uSeq));
     }
 
     @Operation(summary = "스터디 모집 게시글 삭제", description = "스터디 모집 게시글 삭제 API 입니다.")
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteRecruitmentBoard(@PathVariable Long id, @RequestParam String uSeq){
-        recruitmentBoardService.deleteRecruitmentBoard(id, uSeq);
+    @DeleteMapping("/{rb_seq}")
+    public ResponseEntity<Void> deleteRecruitmentBoard(@PathVariable Long rb_seq, @RequestParam String uSeq) {
+        recruitmentBoardService.deleteRecruitmentBoard(rb_seq, uSeq);
         return ResponseEntity.noContent().build();
     }
+
+    @Operation(summary = "내가 그룹장인 스터디 그룹 리스트", description = "내가 그룹장인 스터디 그룹 리스트 API 입니다.")
+    @GetMapping("/myStudyGroups")
+    public ResponseEntity<List<StudyGroupDTO>> getMyStudyGroups(@AuthenticationPrincipal String uSeq) {
+        List<StudyGroupDTO> studyGroups = recruitmentBoardService.getMyStudyGroups(Long.parseLong(uSeq));
+        if(studyGroups.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }else {
+            return ResponseEntity.ok(studyGroups);
+        }
+    }
+    // 1. 내가 그룹장인 스터디 그룹 보여주기 전에 이미 모집 게시판에 존재하는 스터디 그룹 제외하는 if문 추가하기
+    
 }
--- server/src/main/java/project/developer_pacemaker/service/RecruitmentBoardService.java
+++ server/src/main/java/project/developer_pacemaker/service/RecruitmentBoardService.java
@@ -3,51 +3,44 @@ package project.developer_pacemaker.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import project.developer_pacemaker.dto.RecruitmentBoardDTO;
+import project.developer_pacemaker.dto.StudyGroupDTO;
 import project.developer_pacemaker.entity.RecruitmentBoardEntity;
 import project.developer_pacemaker.entity.StudyGroupEntity;
+import project.developer_pacemaker.entity.UserEntity;
 import project.developer_pacemaker.repository.RecruitmentBoardRepository;
 import project.developer_pacemaker.repository.StudyGroupRepository;
 import lombok.extern.slf4j.Slf4j;
+import project.developer_pacemaker.repository.UserRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class RecruitmentBoardService {
     private final RecruitmentBoardRepository recruitmentBoardRepository;
     private final StudyGroupRepository studyGroupRepository;
+    private final UserRepository userRepository;
     @Autowired
-    public RecruitmentBoardService(RecruitmentBoardRepository recruitmentBoardRepository, StudyGroupRepository studyGroupRepository) {
+    public RecruitmentBoardService(RecruitmentBoardRepository recruitmentBoardRepository, StudyGroupRepository studyGroupRepository, UserRepository userRepository) {
         this.recruitmentBoardRepository = recruitmentBoardRepository;
         this.studyGroupRepository = studyGroupRepository;
+        this.userRepository = userRepository;
     }
     public RecruitmentBoardEntity createRecruitmentBoard(RecruitmentBoardDTO recruitmentBoardDTO, long uSeq) {
-        // StudyGroupEntity를 이용하여 name을 설정
-//        StudyGroupEntity studyGroup = recruitmentBoard.getSgSeq();
-//        if (studyGroup != null) {
             StudyGroupEntity foundStudyGroup = studyGroupRepository.findById(recruitmentBoardDTO.getSgSeq())
                     .orElseThrow(()-> new RuntimeException("스터디 그룹을 찾을 수 없습니다."));
-
             log.info("studyGroup sgSeq: {}", foundStudyGroup.getSgSeq());
             if(foundStudyGroup.getUser().getUSeq() != uSeq){
                 throw new RuntimeException("사용자가 스터디 그룹장이 아닙니다.");
             }
-//                recruitmentBoard.setTitle(optionalStudyGroup.get().getName())
                 RecruitmentBoardEntity recruitmentBoard = RecruitmentBoardEntity.builder()
                 .studyGroup(foundStudyGroup)
                 .content(recruitmentBoardDTO.getContent())
                 .name(recruitmentBoardDTO.getName())
                 .build();
         return recruitmentBoardRepository.save(recruitmentBoard);
-    }
-
-    public List<RecruitmentBoardEntity> getAllRecruitmentBoards(){
-        return recruitmentBoardRepository.findAll();
-    }
-
-    public List<RecruitmentBoardEntity> getRecruitmentBoardByName(String name){
-        return recruitmentBoardRepository.findByName(name);
     }
 
     public RecruitmentBoardEntity updateRecruitmentBoard(Long id, RecruitmentBoardDTO recruitmentBoardDetails, String uSeq){
@@ -57,17 +50,25 @@ public class RecruitmentBoardService {
             StudyGroupEntity studyGroup = recruitmentBoard.getStudyGroup();
             // 스터디 그룹장이 작성자인지
             if(studyGroup != null && Long.parseLong(uSeq) != studyGroup.getUser().getUSeq()){
-                log.warn("스터디 그룹장이 아닙니다. {}", uSeq);
-                throw new RuntimeException("스터디 그룹의 그룹장이 아닙니다.");
+                log.warn("you are not study group leader. {}", uSeq);
+                throw new RuntimeException("you are not leader of study group.");
             }
             // if문 통과하면 게시글 업데이트
             recruitmentBoard.setContent(recruitmentBoardDetails.getContent());
+            recruitmentBoard.setName(recruitmentBoardDetails.getName());
             return recruitmentBoardRepository.save(recruitmentBoard);
         }else{
-            throw new RuntimeException("RecruitmentBoard id를 찾을 수 없습니다."+id);
+            throw new RuntimeException("not found RecruitmentBoard id."+id);
         }
     }
 
+    public List<RecruitmentBoardEntity> getAllRecruitmentBoards(){
+        return recruitmentBoardRepository.findAll();
+    }
+
+    public List<RecruitmentBoardEntity> getRecruitmentBoardByName(String name){
+        return recruitmentBoardRepository.findByName(name);
+    }
     public void deleteRecruitmentBoard(Long id, String u_Seq){
         Optional<RecruitmentBoardEntity> optionalRecruitmentBoard = recruitmentBoardRepository.findById(id);
         if(optionalRecruitmentBoard.isPresent()){
@@ -81,5 +82,19 @@ public class RecruitmentBoardService {
         } else {
             throw new RuntimeException("스터디 모집 게시글 id를 찾을 수 없습니다."+id);
         }
+    }
+    public List<StudyGroupDTO> getMyStudyGroups(Long uSeq) {
+        UserEntity user = userRepository.findById(uSeq)
+                .orElseThrow(() -> new RuntimeException("User not found with id " + uSeq));
+        return studyGroupRepository.findByUser(user).stream()
+                .map(studyGroup -> StudyGroupDTO.builder()
+                        .sgSeq(studyGroup.getSgSeq())
+                        .name(studyGroup.getName())
+                        .uSeq(studyGroup.getUser().getUSeq())
+                        .registered(studyGroup.getRegistered())
+                        .goal(studyGroup.getGoal())
+                        .status(studyGroup.isStatus())
+                        .build())
+                .collect(Collectors.toList());
     }
 }
