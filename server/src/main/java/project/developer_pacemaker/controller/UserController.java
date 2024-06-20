bff19524027602aa8af413ff643dee9eed28424e
yhs0329 <scar0329@naver.com>
2024-06-20 18:14:11 +0900

    feat:마이페이지 프로필 조회 API에 useq 추가

--- server/src/main/java/project/developer_pacemaker/controller/UserController.java
+++ server/src/main/java/project/developer_pacemaker/controller/UserController.java
@@ -150,6 +150,7 @@ public class UserController {
                 .img(user.getImg())
                 .nickname(user.getNickname())
                 .social(user.getSocial())
+                .uSeq(user.getUSeq())
                 .build();
 
             return ResponseEntity.ok(userDTO);
